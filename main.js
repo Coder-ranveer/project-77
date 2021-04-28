dbaefb0a6e76a555ca7a78c966d3dd21732384b4
Coder-ranveer <77765040+Coder-ranveer@users.noreply.github.com>
2021-04-27 21:52:30 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("My first console project");
+var firstname = "Ranveer";
+console.log(firstname);
+
+var lastname = "Sarode";
+console.log(lastname);
+
+var fullname = firstname.concat(lastname);
+console.log(fullname);
+var lengthoffullname = fullname.length;
+console.log(lengthoffullname);
+
+var one = 13;
+var two = 2;
+var one =one - two;
+console.log(one);
